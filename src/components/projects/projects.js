10fe6a6c094d858c2d242307816b9e5b8916754c
Kevin Hernandez <kev.hdez05@gmail.com>
2021-12-31 00:09:44 -0400

    vista para celulares ya completa

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -3,8 +3,8 @@ import Doodle from './images/MeditatingDoodle.svg';
 
 export default function projects(props) {
     return (
-        <section class="job-section sm:px-12 md:px-24 lg:px-36 xl:px-96 py-24">
-            <div class="grid grid-cols-2">
+        <section class="job-section md:px-24 lg:px-36 xl:px-96 py-24">
+            <div class="px-7 grid md:grid-cols-2">
                 <div class="px-2">
                     <h1 class="font-semibold">03. Stuff I've Builded</h1>
                     <hr></hr>
