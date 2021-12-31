af726cf9f814dec32b993fee2d308e6c73db63e9
Kevin Hernandez <kev.hdez05@gmail.com>
2021-12-30 23:57:55 -0400

    problema con el responsivo del titulo inicial de la pagina ya arreglado

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,8 +4,8 @@ export default function Header(props){
             <div class="max-w-2xl px-7 md:px-12">
                 
                 <h1 class="text-white-800 text-sm">Howdy, Mate! My name is</h1>
-                <h2 class="text-7xl text-white-600 mt-4 ">{props.name}.</h2>
-                <h2 class="text-5xl text-white-300 mt-4 opacity-60">I Build stuff for the internet.</h2>
+                <h2 class="text-5xl md:text-7xl text-white-600 mt-4 ">{props.name}.</h2>
+                <h2 class="text-3xl md:text-5xl text-white-300 mt-4 opacity-60">I Build stuff for the internet.</h2>
 
                 <h2 class="text-white-800 mt-4 text-2xl opacity-60">
                     I’m a software engineer specializing in building (and occasionally designing) 
