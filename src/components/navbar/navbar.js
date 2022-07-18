d69fdc2942b8d86160cca450250f3a6db21b65f7
Kevin Hernandez <kev.hdez05@gmail.com>
2022-07-17 21:53:26 -0400

    cambio de icono en mi nombre en la pagina

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,9 +8,10 @@ export default function Navbar(props) {
           <div>
             <a class="flex items-center text-gray-800 hover:text-indigo-600" href="#">
 
-              <svg class="h-6 w-6 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              {/* <svg class="h-6 w-6 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-              </svg>
+              </svg> */}
+              <img className="h-6 w-6" src={"./kevin.png"} alt="Kevs icon"></img>
 
               <span class="mx-3 font-medium text-sm md:text-base">{props.name}</span>
             </a>
